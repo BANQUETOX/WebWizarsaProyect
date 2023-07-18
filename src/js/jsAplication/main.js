c8d0dadb17f7e1afc43b4281e96a5e09e13cbbe4
BANQUETOX <BANQUETOX@gmail.com>
2023-07-18 10:24:15 -0600

    folder changes

---
+++ src/js/jsAplication/main.js
@@ -0,0 +1,6 @@
+const SALARYRANGEINPUT = document.getElementById('salary-range-input')
+let salaryRangeValue = document.getElementById('salary-value')
+
+SALARYRANGEINPUT.oninput = ()=>{
+    salaryRangeValue.innerHTML = SALARYRANGEINPUT.value + "₡"
+}
