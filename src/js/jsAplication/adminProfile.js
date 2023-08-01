dcd2ada6f0b4815de6ebf57198c7ba71ae16fce6
BANQUETOX <BANQUETOX@gmail.com>
2023-08-01 11:56:23 -0600

    create job functionality

--- src/js/jsAplication/adminProfile.js
+++ src/js/jsAplication/adminProfile.js
@@ -16,3 +16,74 @@ $("#imageUpload").change(function () {
     readURL(this);
   }
 });
+
+//passwords section
+let passwordForm = document.getElementById("password-form");
+let firstPasswordInput = document.getElementById("first-new-password");
+let secondPasswordInput = document.getElementById("second-new-password");
+let firstErrorLabel = document.getElementById("first-password--message");
+let secondErrorLabel = document.getElementById("second-password--message");
+
+firstPasswordInput.addEventListener("keyup", () => {
+  if (isValidPassword(firstPasswordInput.value)) {
+    firstPasswordInput.classList.add("text-input__valid");
+    firstPasswordInput.classList.remove("text-input__invalid");
+    firstErrorLabel.style.display = "none";
+  } else {
+    firstPasswordInput.classList.add("text-input__invalid");
+    firstPasswordInput.classList.remove("text-input__valid");
+    firstErrorLabel.style.display = "initial";
+  }
+});
+
+firstPasswordInput.addEventListener("blur", () => {
+  if (isValidPassword(firstPasswordInput.value)) {
+    firstPasswordInput.classList.add("text-input__valid");
+    firstPasswordInput.classList.remove("text-input__invalid");
+    firstErrorLabel.style.display = "none";
+  } else {
+    firstPasswordInput.classList.add("text-input__invalid");
+    firstPasswordInput.classList.remove("text-input__valid");
+    firstErrorLabel.style.display = "initial";
+  }
+});
+
+secondPasswordInput.addEventListener("keyup", () => {
+  if (
+    checkTwoPasswords(firstPasswordInput.value, secondPasswordInput.value) &&
+    isValidPassword(firstPasswordInput.value)
+  ) {
+    secondPasswordInput.classList.add("text-input__valid");
+    secondPasswordInput.classList.remove("text-input__invalid");
+    secondErrorLabel.style.display = "none";
+  } else {
+    secondPasswordInput.classList.add("text-input__invalid");
+    secondPasswordInput.classList.remove("text-input__valid");
+    secondErrorLabel.style.display = "initial";
+  }
+});
+
+secondPasswordInput.addEventListener("blur", () => {
+  if (
+    checkTwoPasswords(firstPasswordInput.value, secondPasswordInput.value) &&
+    isValidPassword(firstPasswordInput.value)
+  ) {
+    secondPasswordInput.classList.add("text-input__valid");
+    secondPasswordInput.classList.remove("text-input__invalid");
+    secondErrorLabel.style.display = "none";
+  } else {
+    secondPasswordInput.classList.add("text-input__invalid");
+    secondPasswordInput.classList.remove("text-input__valid");
+    secondErrorLabel.style.display = "initial";
+  }
+});
+
+passwordForm.addEventListener("submit", (e) => {
+  if (
+    checkTwoPasswords(firstPasswordInput.value, secondPasswordInput.value) &&
+    isValidPassword(firstPasswordInput.value)
+  ) {
+    window.location.href = "https://github.com/";
+  }
+  e.preventDefault();
+});
--- src/js/jsAplication/createJob.js
+++ src/js/jsAplication/createJob.js
@@ -1,7 +1,34 @@
 const jobForm = document.getElementById("job-creation-form");
 const saveMessage = document.getElementById("saved-changes");
+let inputsCollection = document.getElementsByClassName("job-input");
+let inputsSelectsCollection = document.getElementsByClassName("select");
+
+function verifyJobInputs() {
+  for (let i = 0; i < inputsCollection.length; i++) {
+    if (inputsCollection[i].value == "") {
+      inputsCollection[i].classList.add("job-input__invalid");
+      inputsCollection[i].classList.remove("job-input__valid");
+      if (inputsCollection[i].type == "select-one") {
+        inputsCollection[i].classList.remove("job-input__valid");
+        inputsCollection[i].classList.remove("job-input__invalid");
+        inputsSelectsCollection[i - 2].classList.add("job-input__invalid");
+        inputsSelectsCollection[i - 2].classList.remove("job-input__valid");
+      }
+    } else {
+      inputsCollection[i].classList.add("job-input__valid");
+      inputsCollection[i].classList.remove("job-input__invalid");
+      if (inputsCollection[i].type == "select-one") {
+        inputsCollection[i].classList.remove("job-input__valid");
+        inputsCollection[i].classList.remove("job-input__invalid");
+        inputsSelectsCollection[i - 2].classList.add("job-input__valid");
+        inputsSelectsCollection[i - 2].classList.remove("job-input__invalid");
+      }
+    }
+  }
+}
 
 jobForm.addEventListener("submit", (e) => {
+  verifyJobInputs();
   showSavedChanges(saveMessage);
   e.preventDefault();
 });
