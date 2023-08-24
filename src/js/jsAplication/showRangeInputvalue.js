4b26e063116df808f5b8d1b6ebcbb3a0c573180b
josedanielsteller <josedanielsteller@live.com>
2023-08-23 20:40:48 -0600

    Ultimos, ultimos cambios [?]

--- src/js/jsAplication/showRangeInputvalue.js
+++ src/js/jsAplication/showRangeInputvalue.js
@@ -1,6 +1,11 @@
 const SALARYRANGEINPUT = document.getElementById("salary-range-input");
 let salaryRangeValue = document.getElementById("salary-value");
+let filterRefresh = document.getElementById('filter-refresh')
 
 SALARYRANGEINPUT.oninput = () => {
   salaryRangeValue.innerHTML = SALARYRANGEINPUT.value + "₡";
 };
+
+filterRefresh.addEventListener("click", () => {
+  location.reload()
+})
--- src/js/jsComponents/aplicationFeed.js
+++ src/js/jsComponents/aplicationFeed.js
@@ -1,6 +1,11 @@
 let salaryRange = document.getElementById('salary-range-input')
 let salaryValue = document.getElementById('salary-value')
+let filterRefresh = document.getElementById('filter-refresh')
 
 salaryRange.oninput = ()=>{
     salaryValue.innerHTML = salaryRange.value + "₡"
 }
+
+filterRefresh.addEventListener("click", () => {
+    location.reload()
+});
